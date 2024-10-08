c93285641679f5ea5597b97d421b796d909e0b6c
vijayjumbad <vijayjumbad2016@gmail.com>
2024-10-08 15:25:06 +0530

    spring boot docker app

---
+++ src/main/java/com/example/SpringbootDocker/controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.example.SpringbootDocker.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+	@GetMapping("/hello")
+	public String getMsg() {
+		return "welcome to the docker..!!!";
+	}
+
+}
