ce76a7c37430fe7ad32b18a4d43e5f0b6120b33a
vijayjumbad <vijayjumbad2016@gmail.com>
2024-10-08 17:56:20 +0530

    added jenkins endpoint

--- src/main/java/com/example/SpringbootDocker/controller/HelloController.java
+++ src/main/java/com/example/SpringbootDocker/controller/HelloController.java
@@ -9,5 +9,10 @@ public class HelloController {
 	public String getMsg() {
 		return "welcome to the docker..!!!";
 	}
+	
+	@GetMapping("/jenkins")
+	public String msg() {
+		return "welcome to the docker jenkins integration..!!!";
+	}
 
 }
